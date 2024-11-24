fe2e9cd17aa16095f807a0161d42f8a28410656d
Nasakin Artem <artnas379@gmail.com>
2024-11-24 19:03:56 +0300

    add mixin

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
 
-
-class User(Base):
+class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
