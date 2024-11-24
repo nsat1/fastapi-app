42716f0c219372edd4dc2e91bb3e439f005c27f1
Nasakin Artem <artnas379@gmail.com>
2024-11-24 16:48:12 +0300

    add ApiV1Prefix

--- app/core/config.py
+++ app/core/config.py
@@ -7,8 +7,13 @@ class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    users: str = "/users"
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
