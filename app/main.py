e70c0a2a37b808cc588a7da5034f879b29d47b73
Nasakin Artem <artnas379@gmail.com>
2024-11-24 19:04:09 +0300

    add ORJSONResponse

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
+
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from core.config import settings
 from api import router as api_router
@@ -14,6 +16,7 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 main_app = FastAPI(
+    default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
 main_app.include_router(
