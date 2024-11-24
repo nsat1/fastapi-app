c4c70fc96feef6fb72403805e24c5850612e9fe1
Nasakin Artem <artnas379@gmail.com>
2024-11-24 16:13:39 +0300

    add naming convention

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,8 +1,14 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy import MetaData
 
+from core.config import settings
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
