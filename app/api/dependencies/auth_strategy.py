e9f9daf7e250b41e27aca782babcfa12d4ec5c65
Nasakin Artem <artnas379@gmail.com>
2024-11-24 21:44:17 +0300

    create auth strategy

--- app/api/dependencies/auth_strategy.py
+++ app/api/dependencies/auth_strategy.py
@@ -5,16 +5,19 @@ from fastapi import Depends
 from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
 
 from core.config import settings
+from api.dependencies.access_tokens import get_access_tokens_db
 
 if TYPE_CHECKING:
     from core.models import AccessToken
 
+
 def get_db_strategy(
-        access_token_db: Annotated[AccessTokenDatabase["AccessToken"],
-        Depends(get_access_token_db),
+        access_tokens_db: Annotated[
+            AccessTokenDatabase["AccessToken"],
+            Depends(get_access_tokens_db),
         ],
 ) -> DatabaseStrategy:
     return DatabaseStrategy(
-        access_token_db,
+        database=access_tokens_db,
         lifetime_seconds=settings.access_token.lifetime_seconds,
     )
