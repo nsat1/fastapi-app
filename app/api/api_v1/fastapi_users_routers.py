a74d69900d4597fc730232051fe387eb742f51ef
Nasakin Artem <artnas379@gmail.com>
2024-11-25 19:29:45 +0300

    refactor: move to api_v1

--- app/api/api_v1/fastapi_users_routers.py
+++ app/api/api_v1/fastapi_users_routers.py
@@ -1,8 +1,8 @@
 from fastapi_users import FastAPIUsers
 
 from core.models import User
-from .user_manager import get_user_manager
-from .backend import authentication_backend
+from api.dependencies.auth.user_manager import get_user_manager
+from api.dependencies.auth.backend import authentication_backend
 
 
 fastapi_users = FastAPIUsers[User, int](
