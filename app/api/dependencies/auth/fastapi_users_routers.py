1f24b446d134f5c6a560565b8e2e2107f6f0a939
Nasakin Artem <artnas379@gmail.com>
2024-11-25 19:05:30 +0300

    create fastapi_users_routers.py

---
+++ app/api/dependencies/auth/fastapi_users_routers.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from core.models import User
+from .user_manager import get_user_manager
+from .backend import authentication_backend
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [authentication_backend],
+)
