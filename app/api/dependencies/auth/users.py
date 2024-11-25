c4c44c8aa3dc5258bed4b52d9a7700f91166583b
Nasakin Artem <artnas379@gmail.com>
2024-11-25 17:27:50 +0300

    refactor: move from dp to dp/auth

--- app/api/dependencies/auth/users.py
+++ app/api/dependencies/auth/users.py
@@ -4,9 +4,9 @@ from fastapi import Depends
 
 from core.models import db_helper, User
 
-
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+
 
 async def get_users_db(
     session: Annotated[
