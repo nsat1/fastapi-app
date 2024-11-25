a3e21aa237b70d6e47302912f14923269bae6e51
Nasakin Artem <artnas379@gmail.com>
2024-11-25 22:24:10 +0300

    add registration date to user model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,22 +1,23 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users.db import (
-    SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase,
-)
+from datetime import datetime, timezone
+
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
-
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
-    username: Mapped[str] = mapped_column(unique=True)
+    registration_date: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now(timezone.utc)
+    )
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
