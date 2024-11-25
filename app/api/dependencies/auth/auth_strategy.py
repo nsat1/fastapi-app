13f94448d85168c922295539bda6c3c776e48230
Nasakin Artem <artnas379@gmail.com>
2024-11-25 17:28:18 +0300

    refactor: move from dp to dp/auth

--- app/api/dependencies/auth/auth_strategy.py
+++ app/api/dependencies/auth/auth_strategy.py
@@ -1,21 +1,23 @@
 from typing import TYPE_CHECKING, Annotated
 
 from fastapi import Depends
-
-from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
 
 from core.config import settings
-from api.dependencies.access_tokens import get_access_tokens_db
+from api.dependencies.auth.access_tokens import get_access_tokens_db
 
 if TYPE_CHECKING:
     from core.models import AccessToken
 
 
 def get_db_strategy(
-        access_tokens_db: Annotated[
-            AccessTokenDatabase["AccessToken"],
-            Depends(get_access_tokens_db),
-        ],
+    access_tokens_db: Annotated[
+        AccessTokenDatabase["AccessToken"],
+        Depends(get_access_tokens_db),
+    ],
 ) -> DatabaseStrategy:
     return DatabaseStrategy(
         database=access_tokens_db,
