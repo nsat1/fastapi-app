38895eeb57f6869a17da228430fa0b5c2b489b73
Nasakin Artem <artnas379@gmail.com>
2024-11-28 20:55:06 +0300

    delete registration date

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,11 +1,6 @@
 from typing import TYPE_CHECKING
 
-from datetime import datetime, timezone
-
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DateTime
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
@@ -15,9 +10,6 @@ if TYPE_CHECKING:
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
-    registration_date: Mapped[datetime] = mapped_column(
-        DateTime, default=datetime.now(timezone.utc)
-    )
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
